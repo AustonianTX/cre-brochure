305cc9703551862d1b746475e3667a159fe51836
Austin Johnson <austin@austinjohnson.me>
2023-02-14 16:32:46 -0600

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const port = process.env.PORT ?? 3000;
   console.log(`Listening on port ${port}...`);
   await app.listen(port);
